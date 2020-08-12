d21d12232dabbd6f5baa80c82debe5009554828b
Marc Robotham <forxnives@gmail.com>
2020-08-12 16:52:47 -0700

    working on car

---
+++ src/Components/CartDropdown/CartDropdown.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const CartDropdown = () => {
+    return(
+        <div className="cart-dropdown-container">
+            <div className="cart-dropdown">
+                <span>something</span>
+            </div>
+        </div>
+    )
+}
+
+export default CartDropdown;
--- src/redux/user/user.actions.js
+++ src/redux/user/user.actions.js
@@ -1,6 +1,9 @@
+import { userActionTypes } from '../user.types';
+
+
 const setCurrentUser = user => ({
 
-    type: 'SET_CURRENT_USER',
+    type: userActionTypes.SET_CURRENT_USER,
     payload: user
 
 })
