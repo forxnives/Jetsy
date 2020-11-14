7fe63be53bfe805c556630ee57dbec86371e864a
forxnives <53276787+forxnives@users.noreply.github.com>
2020-11-14 11:42:20 -0500

    Updates

---
+++ precache-manifest.020fb21c118613ee7fe0048d5faf4a92.js
@@ -0,0 +1,438 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bd97558991b5f06725539a68f30e156",
+    "url": "/Jetsy/index.html"
+  },
+  {
+    "revision": "2b30472d28029b784601",
+    "url": "/Jetsy/static/css/2.9f30ecb4.chunk.css"
+  },
+  {
+    "revision": "7a6d84f91cb2950ac2b3",
+    "url": "/Jetsy/static/css/main.e92b6f4d.chunk.css"
+  },
+  {
+    "revision": "2b30472d28029b784601",
+    "url": "/Jetsy/static/js/2.4a71f9b1.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/Jetsy/static/js/2.4a71f9b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a6d84f91cb2950ac2b3",
+    "url": "/Jetsy/static/js/main.20e24b45.chunk.js"
+  },
+  {
+    "revision": "a9de4fe9a5403c11dc71",
+    "url": "/Jetsy/static/js/runtime-main.01df1453.js"
+  },
+  {
+    "revision": "4e81d3d1336e24f1d7bfdf7cd8585a8e",
+    "url": "/Jetsy/static/media/bandanasoft.4e81d3d1.png"
+  },
+  {
+    "revision": "84427bc3b7153c220c6a2190bd4cf832",
+    "url": "/Jetsy/static/media/baseballcap.84427bc3.png"
+  },
+  {
+    "revision": "2baf21ab28bba820e44eefc506df9000",
+    "url": "/Jetsy/static/media/basketballjersey.2baf21ab.png"
+  },
+  {
+    "revision": "cf6dbdfcfaf09364f33ac7c65b2d688d",
+    "url": "/Jetsy/static/media/bird_pillow.cf6dbdfc.jpg"
+  },
+  {
+    "revision": "3510d5022fa769f89b700a2b348c770c",
+    "url": "/Jetsy/static/media/blackdreadextension.3510d502.jpg"
+  },
+  {
+    "revision": "451c5b2e2d1b43cac7687edaab863686",
+    "url": "/Jetsy/static/media/boxinggloves.451c5b2e.jpg"
+  },
+  {
+    "revision": "86ccfc9c2abcf3c5012f9c3efff5d503",
+    "url": "/Jetsy/static/media/bullet (2).86ccfc9c.svg"
+  },
+  {
+    "revision": "02ba5644c3cc838c788913c265523d8c",
+    "url": "/Jetsy/static/media/bumpersticker.02ba5644.jpg"
+  },
+  {
+    "revision": "cfb7dcf3a65f10f38623c6740bcffdb5",
+    "url": "/Jetsy/static/media/cafebluecoffee.cfb7dcf3.jpg"
+  },
+  {
+    "revision": "4567239dbbb1cf2da72de12fb7996341",
+    "url": "/Jetsy/static/media/calabash_container.4567239d.jpg"
+  },
+  {
+    "revision": "dfc11f744f955d52d203966c75c45633",
+    "url": "/Jetsy/static/media/cartoon_pins.dfc11f74.jpg"
+  },
+  {
+    "revision": "02263ce08b031963d55deeecaf0cd9e0",
+    "url": "/Jetsy/static/media/castorhairoil.02263ce0.jpg"
+  },
+  {
+    "revision": "0dc3f57f5bccf5f5fa42aabc79f7e0ea",
+    "url": "/Jetsy/static/media/coffeeherocropped.0dc3f57f.jpg"
+  },
+  {
+    "revision": "3eceaa168438fad1e47a312edef5221f",
+    "url": "/Jetsy/static/media/coolingsleeves.3eceaa16.jpg"
+  },
+  {
+    "revision": "0f105aec82066e4ddb854e6f70d82adb",
+    "url": "/Jetsy/static/media/costumesunglasses.0f105aec.jpg"
+  },
+  {
+    "revision": "c5073728c85e903f22006b87247bd977",
+    "url": "/Jetsy/static/media/cottonbalaclava.c5073728.png"
+  },
+  {
+    "revision": "82d2dbfa59e84b93ed19cd251cc98c60",
+    "url": "/Jetsy/static/media/cyclingjersey.82d2dbfa.jpg"
+  },
+  {
+    "revision": "6d4963d9203757cd4fa695098a7e3451",
+    "url": "/Jetsy/static/media/cyclingjerseywomen.6d4963d9.jpg"
+  },
+  {
+    "revision": "1992c55c349e6d9b04c404acbdde3702",
+    "url": "/Jetsy/static/media/deskflag.1992c55c.jpg"
+  },
+  {
+    "revision": "1263fbd278a7a9e9e060e53d72384d48",
+    "url": "/Jetsy/static/media/dominotable.1263fbd2.jpg"
+  },
+  {
+    "revision": "5eb38c1f8cb0a9b21e79020d0db4dce8",
+    "url": "/Jetsy/static/media/dreadextensions.5eb38c1f.jpg"
+  },
+  {
+    "revision": "a437c230d3c6c24fa64c7f10f3002dde",
+    "url": "/Jetsy/static/media/ethiopiaearrings.a437c230.jpg"
+  },
+  {
+    "revision": "0fc265617dfc03b3095feae7da0d9ce2",
+    "url": "/Jetsy/static/media/female_tshirt.0fc26561.jpg"
+  },
+  {
+    "revision": "e0a21500c7234e3e4a294c913f91d18e",
+    "url": "/Jetsy/static/media/femalelongshirt.e0a21500.png"
+  },
+  {
+    "revision": "592682d782e8c7c7b6d0537c59957b3b",
+    "url": "/Jetsy/static/media/flag3x5.592682d7.jpg"
+  },
+  {
+    "revision": "9f0b232e349b5874a41091ae2b5db190",
+    "url": "/Jetsy/static/media/flagblanket.9f0b232e.jpg"
+  },
+  {
+    "revision": "443c9d904bf04552c2a3ff50c3296289",
+    "url": "/Jetsy/static/media/flagboxers.443c9d90.png"
+  },
+  {
+    "revision": "37609ddcc49729af2a42ec1345edbcb5",
+    "url": "/Jetsy/static/media/flagboxinggloves.37609ddc.jpg"
+  },
+  {
+    "revision": "5c1cf3ebf3ebd9965c45974876451c9f",
+    "url": "/Jetsy/static/media/flagbrassgrommets.5c1cf3eb.jpg"
+  },
+  {
+    "revision": "40fbb88317eddfcddd3a715f60a44708",
+    "url": "/Jetsy/static/media/flaglicense.40fbb883.jpg"
+  },
+  {
+    "revision": "3bcbcafbba88335ebc56e0b41a870544",
+    "url": "/Jetsy/static/media/flagpatches.3bcbcafb.jpg"
+  },
+  {
+    "revision": "31a9345a2315c034ad2675a2756b7306",
+    "url": "/Jetsy/static/media/floralonepiece.31a9345a.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Jetsy/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Jetsy/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Jetsy/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Jetsy/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Jetsy/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "0a3849a8698186967399ffa777023446",
+    "url": "/Jetsy/static/media/footballscarf.0a3849a8.png"
+  },
+  {
+    "revision": "d163603dd909d1e3c4f5dbc57708a037",
+    "url": "/Jetsy/static/media/fridgemagnet.d163603d.jpg"
+  },
+  {
+    "revision": "d4b22da9f583da7064bb75b6edb4116b",
+    "url": "/Jetsy/static/media/goodvibesmenshirt.d4b22da9.png"
+  },
+  {
+    "revision": "2601ea968f08a6f7c76cf824cd7aba70",
+    "url": "/Jetsy/static/media/goodvibeswomenshirt.2601ea96.png"
+  },
+  {
+    "revision": "02520f77dcd1bd1d3faf1303216265c7",
+    "url": "/Jetsy/static/media/guitarstrap.02520f77.jpg"
+  },
+  {
+    "revision": "500060f8ea5bf01fee0cf1b27ef713a4",
+    "url": "/Jetsy/static/media/hammock.500060f8.jpg"
+  },
+  {
+    "revision": "dcb68c3350c1e17a5083632ad5d6c95d",
+    "url": "/Jetsy/static/media/handflags.dcb68c33.jpg"
+  },
+  {
+    "revision": "5d94a02a86a1edaa81902e56ad471525",
+    "url": "/Jetsy/static/media/haymancoffee.5d94a02a.jpg"
+  },
+  {
+    "revision": "a0f03336cb52e25e57a2e5e321ced123",
+    "url": "/Jetsy/static/media/haymancoffeepods.a0f03336.jpg"
+  },
+  {
+    "revision": "bd2ef5afafc860b33027bfea8afae8b8",
+    "url": "/Jetsy/static/media/hero_couple.bd2ef5af.jpg"
+  },
+  {
+    "revision": "3851abad2053367f4a8a2a832a8bb868",
+    "url": "/Jetsy/static/media/hooragflag.3851abad.jpg"
+  },
+  {
+    "revision": "6341f6d3f3a8fe9618326f17a8818cbd",
+    "url": "/Jetsy/static/media/islandspicejerk.6341f6d3.jpg"
+  },
+  {
+    "revision": "7b755d894bddfd12ff13cf5c8e7bcd02",
+    "url": "/Jetsy/static/media/jablumcoffee.7b755d89.jpg"
+  },
+  {
+    "revision": "bd8de76ae3eaf438375a1a741ee84af5",
+    "url": "/Jetsy/static/media/jablumpremium.bd8de76a.jpg"
+  },
+  {
+    "revision": "0ec64e34a15f0ee526fdc66acbf6b687",
+    "url": "/Jetsy/static/media/jamaica_heart.0ec64e34.jpg"
+  },
+  {
+    "revision": "856c6fe0d8feaf6d2391e03c223a80cf",
+    "url": "/Jetsy/static/media/jamaicachain.856c6fe0.png"
+  },
+  {
+    "revision": "f142a8c0ff95d21ae8759d1c72af6520",
+    "url": "/Jetsy/static/media/jamaicamistake.f142a8c0.jpg"
+  },
+  {
+    "revision": "7778a3bf451c40b6e56ad93f637ac744",
+    "url": "/Jetsy/static/media/jewellryherocropped.7778a3bf.jpg"
+  },
+  {
+    "revision": "10572e1bdcdaf43f884ecbf6cbba681b",
+    "url": "/Jetsy/static/media/kingstonparadise.10572e1b.jpg"
+  },
+  {
+    "revision": "1b929d090b081e0159fb72b1f718d54d",
+    "url": "/Jetsy/static/media/lemonpepperspice.1b929d09.jpg"
+  },
+  {
+    "revision": "c6deaa05c6955a70111b0beee7af1883",
+    "url": "/Jetsy/static/media/lionsunshade.c6deaa05.jpg"
+  },
+  {
+    "revision": "e3f95f5c281d416a69da4f7f258bd30a",
+    "url": "/Jetsy/static/media/liquidextract.e3f95f5c.jpg"
+  },
+  {
+    "revision": "da46229d3c2d9b9d5545fc53f7e84405",
+    "url": "/Jetsy/static/media/logo.da46229d.jpg"
+  },
+  {
+    "revision": "0cc00b5fc0b061e698f43f68cf5d74d5",
+    "url": "/Jetsy/static/media/logobare.0cc00b5f.png"
+  },
+  {
+    "revision": "4bbb7555062190f1d76668abe54e00c6",
+    "url": "/Jetsy/static/media/longsleevezipup.4bbb7555.png"
+  },
+  {
+    "revision": "b0c34f890c89e91332ac4558ac6e372d",
+    "url": "/Jetsy/static/media/magnumexotics.b0c34f89.jpg"
+  },
+  {
+    "revision": "065fab81d8f059ffd905637176f98f29",
+    "url": "/Jetsy/static/media/male_tshirt.065fab81.png"
+  },
+  {
+    "revision": "5cc7aece89cdf679b1f66819585bdb82",
+    "url": "/Jetsy/static/media/mapmagnet.5cc7aece.jpg"
+  },
+  {
+    "revision": "21d00c95a76ee74ed47f34bb7c000da7",
+    "url": "/Jetsy/static/media/maskgirl_hero.21d00c95.png"
+  },
+  {
+    "revision": "734fcb411fe86a705ada88297e204549",
+    "url": "/Jetsy/static/media/menclothesherocropped.734fcb41.jpg"
+  },
+  {
+    "revision": "320121b7efdbb53dd4e36c7ab5e6242a",
+    "url": "/Jetsy/static/media/ministringflag.320121b7.jpg"
+  },
+  {
+    "revision": "898a4612285b18f45e7a417d5d45e742",
+    "url": "/Jetsy/static/media/mousepad.898a4612.jpg"
+  },
+  {
+    "revision": "a90966945f6789a60781b3bea590a58d",
+    "url": "/Jetsy/static/media/necklace.a9096694.jpg"
+  },
+  {
+    "revision": "7329194fcc8c2acc40243473bceb2551",
+    "url": "/Jetsy/static/media/onepeoplebook.7329194f.png"
+  },
+  {
+    "revision": "c1f9f38ba53ebc0e739cbc82b5b5f28d",
+    "url": "/Jetsy/static/media/orange_slippers.c1f9f38b.jpg"
+  },
+  {
+    "revision": "f6231a65be2211968152d0f76e5ade18",
+    "url": "/Jetsy/static/media/personalized.f6231a65.png"
+  },
+  {
+    "revision": "d04b2858d7daf7259750e21ddf3d3df8",
+    "url": "/Jetsy/static/media/purpleonepiece.d04b2858.png"
+  },
+  {
+    "revision": "828b7c782845ee5b4897fc362b4e2986",
+    "url": "/Jetsy/static/media/rasta_bag.828b7c78.jpg"
+  },
+  {
+    "revision": "39a955df1083a737dccffb7a00460738",
+    "url": "/Jetsy/static/media/rastabeanie.39a955df.png"
+  },
+  {
+    "revision": "89c46d182a5517d944176e9ca0b0a261",
+    "url": "/Jetsy/static/media/rastabracelet.89c46d18.png"
+  },
+  {
+    "revision": "8d42fdcbf72b9f13be0d6e5fc3c9acf0",
+    "url": "/Jetsy/static/media/rastafishnet.8d42fdcb.png"
+  },
+  {
+    "revision": "7c710b62e279c79a5373727f301a39d1",
+    "url": "/Jetsy/static/media/rastahatdreadlocks.7c710b62.png"
+  },
+  {
+    "revision": "a64b822c8aacaddffa09280475ba819f",
+    "url": "/Jetsy/static/media/rastaknapsack.a64b822c.png"
+  },
+  {
+    "revision": "95be89644deeab233e17f931383ff3d4",
+    "url": "/Jetsy/static/media/rastashirtherocropped.95be8964.jpg"
+  },
+  {
+    "revision": "d5ed4484a7f20e4151049f4cd87f4c8c",
+    "url": "/Jetsy/static/media/rastaumbrelladress.d5ed4484.png"
+  },
+  {
+    "revision": "63a704ce5e8afae720f0cd63ac1b9dda",
+    "url": "/Jetsy/static/media/skulldecal.63a704ce.jpg"
+  },
+  {
+    "revision": "0932b5bf995e62d1891509871b6f939c",
+    "url": "/Jetsy/static/media/sprintermovie.0932b5bf.jpg"
+  },
+  {
+    "revision": "487d435aa954c58dc489e1c74f030e12",
+    "url": "/Jetsy/static/media/steellicense.487d435a.jpg"
+  },
+  {
+    "revision": "e3db518b9511268b9c4b5b77a152a547",
+    "url": "/Jetsy/static/media/stripe-secure.e3db518b.png"
+  },
+  {
+    "revision": "2c125580a456e11a946bae7260bc6fb4",
+    "url": "/Jetsy/static/media/surfgirls.2c125580.jpg"
+  },
+  {
+    "revision": "0dc9473b5b2df16c357525a21134dcf4",
+    "url": "/Jetsy/static/media/tea_set.0dc9473b.jpg"
+  },
+  {
+    "revision": "c1a85aa3203ad0ee367f038fdaaccb43",
+    "url": "/Jetsy/static/media/thickflag.c1a85aa3.jpg"
+  },
+  {
+    "revision": "7c232dd55b776c79d3ad7d767ac6d0fa",
+    "url": "/Jetsy/static/media/tintin.7c232dd5.png"
+  },
+  {
+    "revision": "265831e4fbba0d377bf4e3d452e287f8",
+    "url": "/Jetsy/static/media/torsty.265831e4.png"
+  },
+  {
+    "revision": "55f4872e7dc4f636e0570d9fe56d4805",
+    "url": "/Jetsy/static/media/ultimatetour.55f4872e.jpg"
+  },
+  {
+    "revision": "fa26243ea88d86ae3a9e88268b32adee",
+    "url": "/Jetsy/static/media/umbrohomejersey.fa26243e.png"
+  },
+  {
+    "revision": "acd7c59e38db48f968a58b8ca6e9d5f8",
+    "url": "/Jetsy/static/media/umbrojersey.acd7c59e.jpg"
+  },
+  {
+    "revision": "4de2dd84fc36ce2d7c702940b4c99922",
+    "url": "/Jetsy/static/media/vinyldecal.4de2dd84.jpg"
+  },
+  {
+    "revision": "355d075c7942e3902cd6ada2e85f76f4",
+    "url": "/Jetsy/static/media/wedding_gift.355d075c.png"
+  },
+  {
+    "revision": "591c347aa8be1d59d045083f2b877e55",
+    "url": "/Jetsy/static/media/weedshower.591c347a.jpg"
+  },
+  {
+    "revision": "f07e5bf6eff5b14f66e7e19e0eb85948",
+    "url": "/Jetsy/static/media/windowflag.f07e5bf6.jpg"
+  },
+  {
+    "revision": "8ad60efcc6ad4a516da42f2c3e642fe8",
+    "url": "/Jetsy/static/media/wood_bracelet.8ad60efc.jpg"
+  },
+  {
+    "revision": "3aef0f3f92176959e6bc10780bfd9f41",
+    "url": "/Jetsy/static/media/wood_spoons.3aef0f3f.jpg"
+  },
+  {
+    "revision": "cedbcc1e44d48526f86927d845f86c13",
+    "url": "/Jetsy/static/media/yardiemovie.cedbcc1e.jpg"
+  },
+  {
+    "revision": "4b19a8fd914bad618f749ab352ae92d7",
+    "url": "/Jetsy/static/media/yellowonepiece.4b19a8fd.png"
+  }
+]);
